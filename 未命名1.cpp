6b1c2ec550ad09ee252b804089328f03c99b1df3
YOUNG MIKUMA <erwinrommel0505@gmail.com>
2023-09-22 10:34:42 +0800

    [add]sep22th

---
+++ 未命名1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("  长歌行\n");
+    printf("  汉乐府\n\n");
+    printf("青青园中葵，\n");
+    printf("朝露待日晞。\n");
+    printf("阳春布德泽，\n");
+    printf("万物生光辉。\n");
+    printf("常恐秋节至，\n");
+    printf("焜黄华叶衰。\n");
+    printf("常恐秋节至，\n");
+    printf("焜黄华叶衰。\n");
+    printf("少壮不努力，\n");
+    printf("老大徒伤悲！\n");
+    return 0;
+}
+
---
+++ 未命名2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    printf("  长歌行\n");
+    printf("  汉乐府\n");
+    printf("         \n");
+    printf("青青园中葵。\n");
+    printf("朝露待日晞。\n");
+    printf("阳春布德泽，\n");
+    printf("万物生春晖。\n");
+    printf("常恐秋节至，\n");
+    printf("焜黄华叶衰。\n");
+    printf("百川东到海，\n");
+    printf("何时复西归？\n");
+    printf("少壮不努力，\n");
+    printf("老大徒伤悲！\n");
+    return 0;
+}
+
